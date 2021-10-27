4a991db9a1eb1794cf6a1999f6abc32e593835c3
Scott Hillier <scotthhillier@gmail.com>
2021-10-27 13:52:19 -0700

    Returns an array of each position for each letter

---
+++ letterPositions.js
@@ -0,0 +1,22 @@
+const letterPositions = function (sentence) {
+  const result = {};
+  let x = 0;
+  array = [];
+  for (const item of sentence) {
+    if (item === " ") {
+      x += 1;
+      continue;
+    } else if (result[item]) {
+      result[item].push(x);
+    } else {
+      result[item] = [];
+      result[item].push(x);
+    }
+    x += 1;
+  }
+  return result;
+};
+
+const sentence = "lighthouse in the house";
+
+console.log(letterPositions(sentence));
