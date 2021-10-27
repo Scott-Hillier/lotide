f7f04e04611218a3544c9850ca1611fca0dc98c2
Scott Hillier <scotthhillier@gmail.com>
2021-10-27 08:08:31 -0700

    Flatten Arryas

---
+++ flatten.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assert Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assert Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (array1, array2) {
+  if (JSON.stringify(array1) === JSON.stringify(array2)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const flatten = function (array) {
+  const x = array.flat();
+  return x;
+};
