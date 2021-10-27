ec73a4588d0f160ccdd430700408436df2a06287
Scott Hillier <scotthhillier@gmail.com>
2021-10-27 14:41:05 -0700

    Checks if the input matches a key and then returns the key

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assert Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assert Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, input) {
+  for (const key in object) {
+    if (object[key] === input) {
+      console.log(key);
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
